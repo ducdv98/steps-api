ac5ca825f9967bebe1551c3172344b7c58cdcc0d
Duc Dao <ducdv092@gmail.com>
2024-09-05 12:52:00 +0700

    Initial commit

---
+++ src/main/java/com/ducdv/stepsapi/StepsApiApplication.java
@@ -0,0 +1,13 @@
+package com.ducdv.stepsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ducdv/stepsapi/StepsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducdv.stepsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
